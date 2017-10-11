822e56fe4516332749b8124d763100aaf4774259
Gergely Lovas <gergely.lovas@logmein.com>
2017-10-11 16:39:21 +0200

    Add array slice and read functions to wasm

--- jpeg-decode.ts
+++ jpeg-decode.ts
@@ -1,5 +1,5 @@
-let inBytes: usize;
-let bytesSize: i32;
+let inputStartPointer: usize;
+let inputSize: i32;
 let resultSize: i32 = 100;
 const dctZigZag : i32[] = [
     0,
@@ -18,15 +18,65 @@ const dctZigZag : i32[] = [
     55, 62,
     63
 ];
+var dctCos1 : u32  =  4017   // cos(pi/16)
+var dctSin1 : u32  =   799   // sin(pi/16)
+var dctCos3 : u32  =  3406   // cos(3*pi/16)
+var dctSin3 : u32  =  2276   // sin(3*pi/16)
+var dctCos6 : u32  =  1567   // cos(6*pi/16)
+var dctSin6 : u32  =  3784   // sin(6*pi/16)
+var dctSqrt2 : u32 =  5793   // sqrt(2)
+var dctSqrt1d2 : u32 = 2896  // sqrt(2) / 2
+
+function sliceUint8Array (array : Uint8Array, offset: i32, length: i32) : Uint8Array {
+    let result : Uint8Array = new Uint8Array(length);
+    for(let i : i32 = 0; i < offset + length; i++) {
+        if(offset + i >= array.length) {
+            break;
+        }
+        result[i] = array[offset+i];
+    }
+
+    return result;
+}
 
 function generateResult() : usize {
     let resultPosition : usize = malloc(resultSize);
     for(let i : i32 = 0; i < resultSize; i++) {
-        let currentByte : u8 = load<u8>(inBytes+i);
+        let currentByte : u8 = load<u8>(inputStartPointer+i);
         currentByte++;
         store<u8>(resultPosition+i, currentByte);
     }
     return resultPosition;
+}
+
+export function readUint16(data : Uint8Array, offset : i32) : u16 {
+    let value : u16 = (data[offset] << 8) | data[offset + 1];
+    return value;
+} // !!!! increment offset by 2 after call
+
+export function readDataBlock(data : Uint8Array, offset : i32) : Uint8Array {
+    let length : i32  = readUint16(data, offset) as i32;
+    let array : Uint8Array =sliceUint8Array(data, offset, length - 2);
+    return array;
+} // !!!! increment offset with lenght after call
+
+
+class Jfif {
+    majorVersion: u8;
+    minorVersion: u8;
+    densityUnits: u8;
+    xDensity: u16;
+    yDensity: u16;
+    thumbWidth: u8;
+    thumbHeight: u8;
+    thumbData: Uint8Array;
+}
+
+class Adobe {
+    version : u8;
+    flags0 : u16;
+    flags1 : u16;
+    transformCode : u8;
 }
 
 export function getResultSize() : i32 {
@@ -34,9 +84,9 @@ export function getResultSize() : i32 {
 }
 
 export function getInOffset(size: i32) : usize {
-    bytesSize = size;
-    inBytes = malloc(size);
-    return inBytes;
+    inputSize = size;
+    inputStartPointer = malloc(size);
+    return inputStartPointer;
 }
 
 export function jpeg_decode() : usize {
